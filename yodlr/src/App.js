0a84d201037a31aa0aafda59e13b4efeaa13e129
kelly <sntsklvn@gmail.com>
2023-09-12 11:19:02 -0400

    made css styles for registerform

--- yodlr/src/App.js
+++ yodlr/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import RoutePaths from './Routes';
+
+
+
+
+
 function App() {
   return (
     <div className="App">
      <RoutePaths/>
+     
     </div>
   );
 }
--- yodlr/src/RegisterForm.js
+++ yodlr/src/RegisterForm.js
@@ -1,6 +1,8 @@
 import React from "react";
 import {useState} from "react";
 import { useNavigate } from "react-router-dom";
+import "./RegisterForm.css";
+
 
 const RegisterForm = ({onRegister})=>{
 
@@ -35,43 +37,52 @@ const RegisterForm = ({onRegister})=>{
 
     return(
         <>
-        <h1> Sign Up </h1>
+        <h1 class="display-4" > Register to Yodlr</h1>
 
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="firstName" > First Name</label>
+        <div className="form-container">
+            <form  onSubmit={handleSubmit}>
+
+                <label className="form-label" htmlFor="firstName" > First Name</label>
                     <input
+                    className="form-input"
                     type="text"
                     id="firstName"
                     name="firstName"
                     value={formData.firstName}
                     onChange={handleChange}
                 />
-                <label htmlFor="lastName">Last Name</label>
+
+                <label className="form-label" htmlFor="lastName">Last Name</label>
                 <input
+                className="form-input"
                 type="text"
                 id="lastName"
                 name="lastName"
                 value={formData.lastName}
                 onChange={handleChange}
                 />
-                  <label htmlFor="email">Email</label>
+
+                <label className="form-label" htmlFor="email">Email</label>
                 <input
+                className="form-input"
                 type="text"
                 id="email"
                 name="email"
                 value={formData.email}
                 onChange={handleChange}
                 />
-                  <label htmlFor="state">State</label>
+
+                  <label className="form-label" htmlFor="state">State</label>
                 <input
+                className="form-input"
                 type="text"
                 id="state"
                 name="state"
                 value={formData.state}
                 onChange={handleChange}
                 />
-                <button>Submit</button>
+
+                <button className="form-button">Submit</button>
             </form>
         </div>
         </>
