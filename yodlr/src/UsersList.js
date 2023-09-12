4d80b5684aed2a42db6648458cf9046e9ef7a505
kelly <sntsklvn@gmail.com>
2023-09-12 11:34:46 -0400

    added styles to UserList

--- yodlr/src/UsersList.js
+++ yodlr/src/UsersList.js
@@ -1,32 +1,37 @@
 import React from "react";
 import { useEffect, useState } from 'react';
 import User from "./User";
-
+import "./UsersList.css"
 const UserList = ({allUsers})=>{
 
     const [users, setUsers] = useState(allUsers);
-    const [error, setError] = useState(null);
-    console.log(users)
+
     
     useEffect(()=>{
       setUsers(allUsers)
     }, [allUsers])
   
     return (
-      <div >
-        <h1>Users</h1>
-
-        {users.length === 0? (
+      <>
+      <h1 class="display-4">Users</h1>
+      <div className="user-list-container">
+        
+  
+        {users.length === 0 ? (
           <p>Loading...</p>
         ) : (
           <ul>
             {users.map((user) => (
-             <User firstName={user.firstName} lastName={user.lastName} email={user.email}/>
+              <div className="user-item" key={user.id}>
+                <User firstName={user.firstName} lastName={user.lastName} email={user.email} />
+              </div>
             ))}
           </ul>
         )}
       </div>
+      </>
     );
-  }
+  };
+  
 
 export default UserList;
